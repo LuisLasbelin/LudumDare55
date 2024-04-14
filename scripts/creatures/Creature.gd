0c313d52553ade9d98f99e1376eee0437af8837a
Luis Belloch <luis.bellochmartinez@gmail.com>
2024-04-14 20:44:22 +0200

    FEAT: acid mana extra

--- scripts/creatures/Creature.gd
+++ scripts/creatures/Creature.gd
@@ -17,7 +17,9 @@ func get_mana() -> int:
 	var multiplier = 1
 	var added = 0
 	if effects.has("zap"):
-		multiplier += 0.2
-	if effects.has("zap") and effects.has("ice"):
 		multiplier += 1
+		if effects.has("ice"):
+			multiplier += 1
+	if effects.has("acid") and not effects.has("ice"):
+		added += 50
 	return ceil((data.mana_on_sacrifice + added) * multiplier)
