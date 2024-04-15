de3d45309a077565eded7ddc4decd88032281f3c
Luis Belloch <luis.bellochmartinez@gmail.com>
2024-04-15 21:55:07 +0200

    FEAT: shader orb

--- scripts/StartGame.gd
+++ scripts/StartGame.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var mana_objective : float = 500.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +11,8 @@ func _ready():
 	GameState.draw_card()
 	GameState.draw_card()
 
+
+func _process(delta):
+	$Orb/BolaMana.material.set_shader_parameter("mana", GameState.current_mana / mana_objective)
+	if GameState.current_mana >= mana_objective:
+		$EndGameText.visible = true
